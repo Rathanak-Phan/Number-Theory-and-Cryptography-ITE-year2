bb1c32bfd876925801eaf01db592324c974b4641
Rathanak <studyingalaxy123@gmail.com>
2025-05-31 23:56:37 +0700

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<script></script>
+
+<template>
+    <header class="w-full px-auto border-2 py-20">
+        <div class="w-6/10 mx-auto">
+        <div class="flex items-center justify-center">
+            <img class="w-[150px] h-[150px]" src="/logo.png" alt="RUPP logo" />
+            <div>
+            <p class="text-5xl moul-regular font-bold">
+                សាកលវិទ្យាល័យភូមិន្ទភ្នំពេញ
+            </p>
+            <p class="text-4xl playwrite font-bold mx-15">
+                Royal University of Phnom Penh
+            </p>
+            </div>
+        </div>
+        <hr />
+        <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem ea
+            similique hic quae quasi fuga aperiam ducimus enim nisi dolore, rerum
+            laboriosam, tempora, provident repellat voluptas maiores architecto vel
+            iste? Laboriosam deserunt temporibus alias fuga ducimus at velit
+            mollitia nisi quo, qui voluptas molestiae labore ut pariatur! Eum, neque
+            tempora! Non mollitia vitae ut? Eum, porro? Fuga, sapiente? Facilis
+            totam tenetur quae architecto, non assumenda iure deserunt ab vero error
+            explicabo odit est incidunt nulla. Laudantium similique repellendus aut
+            provident quidem voluptate dolorem, distinctio officiis praesentium
+            deleniti accusantium a autem eum ad temporibus velit porro facere
+            impedit voluptatibus expedita eaque.
+        </div>
+        <hr>
+        </div>
+    </header>
+
+    <main class="w-full px-auto border-2 mt-7 p-10">
+        <h2>Til</h2>
+    </main>
+
+
+
+</template>
