08c14b31287235d51a8b5f27b4fcd18992853705
Rathanak <studyingalaxy123@gmail.com>
2025-06-01 00:30:42 +0700

    Describe what you changed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,67 @@
 <script></script>
 
 <template>
-    <header class="w-full px-auto border-2 py-20">
-        <div class="w-6/10 mx-auto">
-        <div class="flex items-center justify-center m-10">
-            <img class="w-[150px] h-[150px]" src="/logo.png" alt="RUPP logo" />
-            <div>
-            <p class="text-5xl moul-regular font-bold">
-                សាកលវិទ្យាល័យភូមិន្ទភ្នំពេញ
-            </p>
-            <p class="text-4xl playwrite font-bold mx-15">
-                Royal University of Phnom Penh
-            </p>
-            </div>
-        </div>
-        <hr />
+  <header class="relative w-full border-2 py-20">
+    <!-- Background image layer -->
+    <div
+      class="absolute inset-0 bg-[url('https://th.bing.com/th/id/R.77377b033da7e65208486a42e99b1915?rik=8JP1GrPiBVYTZg&riu=http%3a%2f%2f3.bp.blogspot.com%2f_UkrggvWyhFY%2fRpradWn9KWI%2fAAAAAAAAAAM%2fFcxn6T_9rrU%2fw1200-h630-p-k-no-nu%2fDV00001.jpg&ehk=Apj66QhYTtIuIyZWlNLMzigWzT2Fb%2fv3ufCac4boVZo%3d&risl=&pid=ImgRaw&r=0')] bg-cover bg-center opacity-50"
+    ></div>
+
+    <!-- Content layer -->
+    <div class="relative w-6/10 mx-auto text-black">
+      <div class="flex items-center justify-center mb-10">
+        <img class="w-[150px] h-[150px]" src="/logo.png" alt="RUPP logo" />
         <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem ea
-            similique hic quae quasi fuga aperiam ducimus enim nisi dolore, rerum
-            laboriosam, tempora, provident repellat voluptas maiores architecto vel
-            iste? Laboriosam deserunt temporibus alias fuga ducimus at velit
-            mollitia nisi quo, qui voluptas molestiae labore ut pariatur! Eum, neque
-            tempora! Non mollitia vitae ut? Eum, porro? Fuga, sapiente? Facilis
-            totam tenetur quae architecto, non assumenda iure deserunt ab vero error
-            explicabo odit est incidunt nulla. Laudantium similique repellendus aut
-            provident quidem voluptate dolorem, distinctio officiis praesentium
-            deleniti accusantium a autem eum ad temporibus velit porro facere
-            impedit voluptatibus expedita eaque.
+          <p class="text-5xl moul-regular font-bold">
+            សាកលវិទ្យាល័យភូមិន្ទភ្នំពេញ
+          </p>
+          <p class="text-4xl playwrite font-bold mx-15">
+            Royal University of Phnom Penh
+          </p>
         </div>
-        <hr>
-        </div>
-    </header>
+      </div>
+      <hr />
+      <div>
+        <!-- Your content here -->
+        Lorem ipsum dolor sit amet... Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Magni sed atque quibusdam veritatis amet quisquam
+        voluptas perferendis quos. In sed facilis, dolores totam incidunt magni
+        mollitia beatae id sequi est dolorem alias quae optio laborum. Saepe hic
+        vero quae placeat provident beatae dolore facere quas asperiores veniam
+        autem minima pariatur, quibusdam magnam, nisi ipsum reiciendis animi rem
+        delectus expedita alias tempora laudantium. Tenetur nisi, corporis modi
+        aut officia magnam, natus nostrum magni totam itaque adipisci nihil
+        doloremque sapiente quibusdam! Maiores debitis odit omnis incidunt id
+        quos quia porro. Omnis vero dignissimos quaerat doloribus assumenda
+        fugiat, totam asperiores nam vitae! Earum illum voluptate ad, incidunt
+        adipisci voluptates quas fugiat quam molestias sed doloremque corrupti
+        qui molestiae, assumenda ipsam. Saepe vel quasi facere aut esse nam
+        eligendi. Sunt explicabo optio, repellat soluta expedita ipsa aut ut hic
+        alias id porro numquam eaque modi ea dicta voluptas? Temporibus
+        quibusdam animi eveniet non vitae quasi soluta, quisquam consequuntur
+        modi, enim id perspiciatis amet neque quam? Culpa maxime distinctio
+        cupiditate suscipit veniam recusandae reiciendis, odit perspiciatis
+        accusamus consequatur quam in, quae officiis reprehenderit dolorum autem
+        cumque voluptate eos beatae, perferendis dignissimos. Odit, laudantium?
+        Praesentium mollitia repudiandae quasi dignissimos magni repellendus, at
+        consequatur eveniet quos ratione ipsam est quod eligendi tempore atque
+        enim, nesciunt amet sed illum nobis aspernatur ipsa deleniti iste
+        labore. Eum incidunt quasi, harum quo ipsam molestias corporis
+        voluptatibus quis fuga, expedita explicabo error. Cum nostrum deleniti,
+        minima omnis necessitatibus tempora accusantium blanditiis nulla,
+        voluptatum atque labore incidunt id doloremque, nam itaque consectetur
+        culpa magnam ducimus voluptatem praesentium odit animi harum saepe! Nemo
+        quae dolore ad aspernatur fugit quo aliquid architecto rem ratione ipsa
+        ea, odio, consequatur quibusdam ipsam. Itaque similique praesentium esse
+        repellat quod! Recusandae a temporibus necessitatibus, similique iste
+        quibusdam assumenda obcaecati iusto, exercitationem ipsum laborum modi
+        quisquam, quod debitis nesciunt!
+      </div>
+      <hr />
+    </div>
+  </header>
 
-    <main class="w-full px-auto border-2 mt-7 p-10">
-        <h2>Til</h2>
-    </main>
-
-
-
+  <main class="w-full px-auto border-2 mt-7 p-10">
+    <h2>Til</h2>
+  </main>
 </template>
