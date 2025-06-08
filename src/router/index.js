d9334a0550a009bd63c55dd0857589ab8d9a7027
Rathanak <studyingalaxy123@gmail.com>
2025-06-08 21:04:11 +0700

    Update commite

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,17 @@ import Home from "../views/Home.vue";
 
 const routes = [
   { path: "/", component: Home },
-  {
-    path: "/number-theory",
-    component: () => import("../views/NumberTheory.vue"),
-  },
-  {
-    path: "/cryptography",
-    component: () => import("../views/Cryptography.vue"),
-  },
-  { path: "/concepts", component: () => import("../views/Concepts.vue") },
-  { path: "/demos", component: () => import("../views/Demo.vue") },
-  { path: "/about", component: () => import("../views/About.vue") },
+  // {
+  //   path: "/number-theory",
+  //   component: () => import("../views/NumberTheory.vue"),
+  // },
+  // {
+  //   path: "/cryptography",
+  //   component: () => import("../views/Cryptography.vue"),
+  // },
+  // { path: "/concepts", component: () => import("../views/Concepts.vue") },
+  // { path: "/demos", component: () => import("../views/Demo.vue") },
+  // { path: "/about", component: () => import("../views/About.vue") },
   // {
   //   path: "/demos/:basePath/:hash",
   //   component: () => import("../views/Result.vue"),
