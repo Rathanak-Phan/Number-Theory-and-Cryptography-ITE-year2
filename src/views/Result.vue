cb433395651eb34d08e705e752943b08d92bd6c7
Rathanak <studyingalaxy123@gmail.com>
2025-06-10 15:46:23 +0700

    Update commite

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900"
+    class="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-5"
   >
     <h1 class="text-4xl font-bold mb-6 text-gray-800 dark:text-gray-200">
       Apply for Number Theory & Cryptography
@@ -8,8 +8,8 @@
 
     <img
       src="../assets/option1.png"
-      alt="option1"
-      class="max-w-3/5 h-auto mb-6 rounded-lg shadow-lg"
+      alt="Illustration for option 1"
+      class="max-w-[60%] h-auto mb-6 rounded-lg shadow-lg"
     />
 
     <p class="text-lg text-gray-700 dark:text-gray-300 mb-4">
@@ -17,21 +17,20 @@
       Cryptography.
     </p>
 
-    <p>
+    <p class="text-lg text-gray-700 dark:text-gray-300 mb-4">
       🔗 Original (human-friendly):
       <span class="text-blue-500 break-all">
         https://ite-year2-cryptography.vercel.app/demos/{{ basePath }}/option1
       </span>
     </p>
 
-    <p>
+    <p class="text-lg text-gray-700 dark:text-gray-300 mb-4">
       🔒 Encrypted URL:
       <span class="text-blue-500 break-all">
-        https://ite-year2-cryptography.vercel.app/demos/{{ basePath }}/{{
-          hashHex
-        }}
+        https://ite-year2-cryptography.vercel.app/demos/{{ basePath }}/{{ hashHex }}
       </span>
     </p>
+
     <button
       class="mt-6 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-md transition-colors duration-300"
       @click="goBack"
